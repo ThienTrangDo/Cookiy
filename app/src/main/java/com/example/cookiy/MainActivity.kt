205dd4c9c83d84f3d978c5c0e08ca9377a1b348b
Trang Do <trang93@hotmail.de>
2023-05-17 17:24:16 +0200

    BottomNavMenu eingebaut

--- app/src/main/java/com/example/cookiy/MainActivity.kt
+++ app/src/main/java/com/example/cookiy/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.cookiy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.example.cookiy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(binding.fragmentContainerView.id) as NavHostFragment
+        navController = navHostFragment.navController
+
+        val bottomNavigationView = binding.bottomNavBar
+
+        setupWithNavController(bottomNavigationView, navController)
+
     }
 }
