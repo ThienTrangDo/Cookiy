8fbc679e67b1aafa63fb062949f1fef25e3b8149
Trang Do <trang93@hotmail.de>
2023-06-07 16:23:55 +0200

    Logik Favorit fehlt noch

---
+++ app/src/main/java/com/example/cookiy/data/datamodels/Favorite.kt
@@ -0,0 +1,11 @@
+package com.example.cookiy.data.datamodels
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity (tableName = "favorite_table")
+data class Favorite (
+    @PrimaryKey (autoGenerate = true)
+    val id : Int,
+    val recipeName : String
+    )
